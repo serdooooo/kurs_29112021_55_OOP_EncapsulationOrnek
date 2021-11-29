f599798e144fb23b52f79948c01df5cf88fcdb3b
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-29 19:51:44 +0300

    Add project files.

---
+++ kurs_29112021_55_OOP_EncapsulationOrnek/Daire.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_29112021_55_OOP_EncapsulationOrnek
+{
+    class Daire
+    {
+        //özellikler
+        public double Yaricap { get; set; }
+        private double cap = 0;
+        public double Cap
+        {
+            get
+            {
+                if (Yaricap>0d)
+                {
+                    cap = Yaricap*2;
+                }
+                return cap;
+            }
+        }
+        public double PiSayisi { get; set; }
+        //constructorlar
+        public Daire()
+        {
+            PiSayisi = 3;
+
+        }
+        public Daire(double disardanPiSayisi)
+        {
+            PiSayisi = disardanPiSayisi;
+        }
+        //metotlar
+        public double AlanHesapla()
+        {
+            return PiSayisi * Math.Pow(Yaricap,2);
+        }
+        public double CevreHesapla()
+        {
+            return PiSayisi * Yaricap * 2;
+        }
+
+    }
+}
---
+++ kurs_29112021_55_OOP_EncapsulationOrnek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_29112021_55_OOP_EncapsulationOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Daire isimli bir class'a aşağıdaki şekilde nesne oluşturma kuralları uygulayınız.
+         Pi sayııs olarak bir özellikk tutunuz bu özelliğin değerini 3 olarak atayınız
+            Pi sayısı değerini dışarıdan gönderiniz.
+            bu dairenin çevresini ve alanını hesaplayınız
+         */
+            Daire d1 = new Daire();
+            Console.WriteLine("Yarıcap değerini giriniz");
+            int yaricap = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pi değerini giriniz");
+            double pi = Convert.ToDouble(Console.ReadLine());
+            d1.Yaricap = yaricap;
+            d1.PiSayisi = pi;
+            Console.WriteLine("Dairenin Alanı: "+d1.AlanHesapla());
+            Console.WriteLine("Dairenin Çevresi: "+d1.CevreHesapla());
+            Console.WriteLine("Dairenin capı: "+d1.Cap);
+            Console.ReadLine();
+        }
+    }
+}
